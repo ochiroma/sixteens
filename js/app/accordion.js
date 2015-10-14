d59d9b8bb46358cad6245ebd5c7521a5cfa5e195
Crispioso <crispin.merriman@gmail.com>
2015-10-14 12:15:16 +0100

    Comment out accordion functions

--- js/app/accordion.js
+++ js/app/accordion.js
@@ -73,12 +73,11 @@ $(document).on('ready', function() {
 
 	// If page-load links to footnote, expand accordion and scroll to it
 	//expandAccordion();
-	//window.location = $(this).attr('href');
-
-	// Expand footnotes section if it's collapsed and you click on footnote anchor
-	$('.expand-accordion').on('click', function() {
-		expandAccordion();
-		window.location = $(this).attr('href');
-	});
+	//
+	//// Expand footnotes section if it's collapsed and you click on footnote anchor
+	//$('.expand-accordion').on('click', function() {
+	//	expandAccordion();
+	//	window.location = $(this).attr('href');
+	//});
 
 });
