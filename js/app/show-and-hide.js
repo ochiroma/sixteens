e8f34a7e9ee6bfb3f7ee1d10081359d97f313e13
Jon Dewi Jones <jondewijones@gmail.com>
2015-11-03 11:28:31 +0000

    Updated show/hide to allow an 'is-shown' class

--- js/app/show-and-hide.js
+++ js/app/show-and-hide.js
@@ -1,6 +1,8 @@
 /**
  * Created by crispin on 28/10/2015.
  */
+
+
 //Toggles between showing and hiding content on click. Possible replacement for accordion.js in the future.
 
 // Credit goes to Heydon Pickering (http://heydonworks.com/practical_aria_examples/#progressive-collapsibles)
@@ -12,12 +14,22 @@ $('.js-show-hide .js-show-hide__title').each(function (){
 	// create unique id for a11y relationship
 	var id = 'collapsible-' + $this.index();
 
+	if ($this.hasClass('is-shown')) {
+		var bool = false;
+	} else {
+		var bool = true;
+	}
+
 	// wrap the content and make it focusable
-	$this.nextUntil('.js-show-hide__title').attr({id: id, 'aria-hidden': true});
+	$this.nextUntil('.js-show-hide__title').attr({id: id, 'aria-hidden': bool});
 	var panel = $this.next();
 
+
+	// inverse the boolean value
+	bool = !bool;
+
 	// Add the button inside the title so both the heading and button semantics are read
-	$this.wrapInner('<button aria-expanded="false" aria-controls="'+ id +'">');
+	$this.wrapInner('<button aria-expanded="' + bool +'" aria-controls="' + id + '">');
 	var button = $this.children('button');
 
 	// Toggle the state properties on click
