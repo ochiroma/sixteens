2c467b12fbd551ed1092e09d55677104c365afa5
Jon Dewi Jones <jondewijones@gmail.com>
2015-11-11 16:46:47 +0000

    Update sidebar to work with affix plugin and fix minor bugs

--- js/app/sticky-scroll.js
+++ js/app/sticky-scroll.js
@@ -6,25 +6,37 @@ $(function() {
 
     if ($('.js-sticky-sidebar').length > 0) {
 
-        var $sidebar = $(".js-sticky-sidebar"),
-            $window = $(window),
-            offset = $sidebar.offset(),
-            topPadding = 50;
+        //var $sidebar = $(".js-sticky-sidebar"),
+        //    $window = $(window),
+        //    offset = $sidebar.offset(),
+        //    topPadding = 50;
+        //
+        //
+        //$window.scroll(function () {
+        //    if ($window.scrollTop() > offset.top) {
+        //        $sidebar.css({
+        //            marginTop: $window.scrollTop() - offset.top + topPadding
+        //        });
+        //    } else {
+        //        $sidebar.stop().animate({
+        //            marginTop: 0
+        //        });
+        //    }
+        //});
 
+        // kick in twbs affix plugin
 
-        $window.scroll(function () {
-            if ($window.scrollTop() > offset.top) {
-                $sidebar.css({
-                    marginTop: $window.scrollTop() - offset.top + topPadding
-                });
-            } else {
-                $sidebar.stop().animate({
-                    marginTop: 0
-                });
+        $(".js-sticky-sidebar").affix({
+            offset: {
+                top: function () {
+                    return (this.top = $('header').outerHeight(true) + $('.page-intro').outerHeight(true))
+                }
             }
         });
 
     }
+
+
 
 });
 
@@ -47,54 +59,80 @@ $.fn.isOnScreen = function(){
 
 };
 
+//$.fn.percentOnScreen = function(){
+//
+//    var win = $(window);
+//
+//    var viewport = {
+//        top : win.scrollTop(),
+//        bottom : win.scrollTop() + win.height()
+//    };
+//
+//    var el = this,
+//        percentage = 250,
+//        elHeight = el.height(),
+//        offsetTop = el.offset().top,
+//        offsetBottom = el.offset().top + elHeight;
+//
+//
+//    if (viewport.top >= offsetTop && viewport.top < offsetBottom) {
+//        percentage = ((offsetBottom - viewport.top) / elHeight) * 100;
+//    } else if (viewport.bottom > offsetTop && viewport.bottom < offsetBottom) {
+//        percentage = ((viewport.bottom - offsetTop) / elHeight) * 100;
+//        //console.log("(offsetTop: " + offsetTop + "- scrollBottom: " + scrollBottom + ") / sectionHeight: " + sectionHeight)
+//    } else if ((viewport.top > offsetTop && viewport.bottom < offsetBottom) || (viewport.top < offsetTop && viewport.bottom > offsetBottom)) {
+//        percentage = 100;
+//    } else {
+//        percentage = 0;
+//    }
+//
+//    return percentage;
+//
+//
+//};
 
 
-    $(window).scroll(function() {
-        var scrollTop = $(window).scrollTop();
-        var scrollBottom = scrollTop + $(window).height();
 
-        $.each($(".section-container"), function() {
+$(window).scroll(function() {
+    var scrollTop = $(window).scrollTop();
+    var scrollBottom = scrollTop + $(window).height();
 
-            var section = $(this),
-                sectionHeight = section.height(),
-                sectionName = section.attr('id'),
-                sectionTitle = section.find('h2').attr('id'),
-                offsetTop = section.offset().top,
-                offsetBottom = section.offset().top + section.height();
+    $.each($(".section-container"), function() {
+
+        var section = $(this),
+            sectionHeight = section.height(),
+            sectionName = section.attr('id'),
+            sectionTitle = section.find('h2').attr('id'),
+            percentage = 0,
+            offsetTop = section.offset().top,
+            offsetBottom = section.offset().top + section.height();
 
 
 
-            if ($(this).isOnScreen()) {
-                var percentage = 0;
+        if ($(this).isOnScreen()) {
 
-                if (scrollTop >= offsetTop && scrollTop < offsetBottom) {
-                    percentage = ((offsetBottom - scrollTop) / sectionHeight) * 100;
-                } else if (scrollBottom > offsetTop && scrollBottom < offsetBottom) {
-                    percentage = ((scrollBottom - offsetTop) / sectionHeight) * 100;
-                    //console.log("(offsetTop: " + offsetTop + "- scrollBottom: " + scrollBottom + ") / sectionHeight: " + sectionHeight)
-                } else if ((scrollTop > offsetTop && scrollBottom < offsetBottom) || (scrollTop < offsetTop && scrollBottom > offsetBottom)) {
-                    percentage = 100;
-                } else {
-                    percentage = 0;
-                }
-
+            if (scrollTop >= offsetTop && scrollTop < offsetBottom) {
+                percentage = ((offsetBottom - scrollTop) / sectionHeight) * 100;
+            } else if (scrollBottom > offsetTop && scrollBottom < offsetBottom) {
+                percentage = ((scrollBottom - offsetTop) / sectionHeight) * 100;
+                //console.log("(offsetTop: " + offsetTop + "- scrollBottom: " + scrollBottom + ") / sectionHeight: " + sectionHeight)
+            } else if ((scrollTop > offsetTop && scrollBottom < offsetBottom) || (scrollTop < offsetTop && scrollBottom > offsetBottom)) {
+                percentage = 100;
             } else {
-
                 percentage = 0;
-
             }
 
-            //console.log($(this).attr('id') + " - " + sectionHeight + ": " + percentage);
-            //console.log(sectionTitle);
+        } else {
 
-            bgTransparency = percentage / 100;
-            //bgTransparency = bgTransparency.toFixed(1);
+            percentage = 0;
 
+        }
 
-            $.each($(".side-bar__item"), function() {
-                $("#" + sectionTitle + "-menu-item").css({'background-color': 'rgba(187,189,191,' + bgTransparency, "border-radius": "8px 0 0 8px"});
+        bgTransparency = percentage / 100;
 
-            });
+        $.each($(".side-bar__item"), function() {
+            $("#" + sectionTitle + "-menu-item").css("background-color", "rgba(187,189,191," + bgTransparency + ")");
+        });
 
     });
 
