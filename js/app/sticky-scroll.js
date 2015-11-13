f56c692c414ec28c0a84c678f1a38a3c8e40389c
Jon Dewi Jones <jondewijones@gmail.com>
2015-11-13 15:09:23 +0000

    Refactored and removed old code

--- js/app/sticky-scroll.js
+++ js/app/sticky-scroll.js
@@ -6,24 +6,6 @@ $(function() {
 
     if ($('.js-sticky-sidebar').length > 0) {
 
-        //var $sidebar = $(".js-sticky-sidebar"),
-        //    $window = $(window),
-        //    offset = $sidebar.offset(),
-        //    topPadding = 50;
-        //
-        //
-        //$window.scroll(function () {
-        //    if ($window.scrollTop() > offset.top) {
-        //        $sidebar.css({
-        //            marginTop: $window.scrollTop() - offset.top + topPadding
-        //        });
-        //    } else {
-        //        $sidebar.stop().animate({
-        //            marginTop: 0
-        //        });
-        //    }
-        //});
-
         // kick in twbs affix plugin
 
         $(".js-sticky-sidebar").affix({
@@ -39,9 +21,9 @@ $(function() {
 
     }
 
-
-
 });
+
+// function to work out if an element is within current viewport
 
 $.fn.isOnScreen = function(){
 
@@ -62,81 +44,63 @@ $.fn.isOnScreen = function(){
 
 };
 
-//$.fn.percentOnScreen = function(){
-//
-//    var win = $(window);
-//
-//    var viewport = {
-//        top : win.scrollTop(),
-//        bottom : win.scrollTop() + win.height()
-//    };
-//
-//    var el = this,
-//        percentage = 250,
-//        elHeight = el.height(),
-//        offsetTop = el.offset().top,
-//        offsetBottom = el.offset().top + elHeight;
-//
-//
-//    if (viewport.top >= offsetTop && viewport.top < offsetBottom) {
-//        percentage = ((offsetBottom - viewport.top) / elHeight) * 100;
-//    } else if (viewport.bottom > offsetTop && viewport.bottom < offsetBottom) {
-//        percentage = ((viewport.bottom - offsetTop) / elHeight) * 100;
-//        //console.log("(offsetTop: " + offsetTop + "- scrollBottom: " + scrollBottom + ") / sectionHeight: " + sectionHeight)
-//    } else if ((viewport.top > offsetTop && viewport.bottom < offsetBottom) || (viewport.top < offsetTop && viewport.bottom > offsetBottom)) {
-//        percentage = 100;
-//    } else {
-//        percentage = 0;
-//    }
-//
-//    return percentage;
-//
-//
-//};
+// function to work out what percentage of an element is within current viewport
+
+$.fn.percentOnScreen = function(){
+
+    var win = $(window);
+
+    var viewport = {
+        top : win.scrollTop(),
+        bottom : win.scrollTop() + win.height()
+    };
+
+    var el = this,
+        elHeight = el.height(),
+        offsetTop = el.offset().top,
+        offsetBottom = el.offset().top + elHeight;
+
+
+    if (viewport.top >= offsetTop && viewport.top < offsetBottom) {
+        percentage = ((offsetBottom - viewport.top) / elHeight) * 100;
+    } else if (viewport.bottom > offsetTop && viewport.bottom < offsetBottom) {
+        percentage = ((viewport.bottom - offsetTop) / elHeight) * 100;
+        //console.log("(offsetTop: " + offsetTop + "- scrollBottom: " + scrollBottom + ") / sectionHeight: " + sectionHeight)
+    } else if ((viewport.top > offsetTop && viewport.bottom < offsetBottom) || (viewport.top < offsetTop && viewport.bottom > offsetBottom)) {
+        percentage = 100;
+    } else {
+        percentage = 0;
+    }
+
+    return percentage;
+
+};
 
 
 
-$(window).on("load scroll",function() {
-    var scrollTop = $(window).scrollTop();
-    var scrollBottom = scrollTop + $(window).height();
+    $(window).on("load scroll", function () {
 
-    $.each($(".section-container"), function() {
+        if ($('body').is('.viewport-lg')) {
 
-        var section = $(this),
-            sectionHeight = section.height(),
-            sectionName = section.attr('id'),
-            sectionTitle = section.find('h2').attr('id'),
-            percentage = 0,
-            offsetTop = section.offset().top,
-            offsetBottom = section.offset().top + section.height();
+            $.each($(".section-container"), function () {
 
+                var section = $(this),
+                    sectionTitle = section.find('h2').attr('id');
 
+                if ($(this).isOnScreen()) {
+                    $(this).percentOnScreen();
 
-        if ($(this).isOnScreen()) {
+                } else {
+                    percentage = 0;
 
-            if (scrollTop >= offsetTop && scrollTop < offsetBottom) {
-                percentage = ((offsetBottom - scrollTop) / sectionHeight) * 100;
-            } else if (scrollBottom > offsetTop && scrollBottom < offsetBottom) {
-                percentage = ((scrollBottom - offsetTop) / sectionHeight) * 100;
-                //console.log("(offsetTop: " + offsetTop + "- scrollBottom: " + scrollBottom + ") / sectionHeight: " + sectionHeight)
-            } else if ((scrollTop > offsetTop && scrollBottom < offsetBottom) || (scrollTop < offsetTop && scrollBottom > offsetBottom)) {
-                percentage = 100;
-            } else {
-                percentage = 0;
-            }
+                }
 
-        } else {
+                bgTransparency = percentage / 100;
 
-            percentage = 0;
-
+                $.each($(".side-bar__item"), function () {
+                    $("#" + sectionTitle + "-menu-item").css("background-color", "rgba(187,189,191," + bgTransparency + ")");
+                });
+            });
         }
-
-        bgTransparency = percentage / 100;
-
-        $.each($(".side-bar__item"), function() {
-            $("#" + sectionTitle + "-menu-item").css("background-color", "rgba(187,189,191," + bgTransparency + ")");
-        });
-
     });
 
-});
