8eac85cc7fd58644f5f797e0f421946e70d0c3dc
Crispioso <crispin.merriman@gmail.com>
2015-11-30 14:36:49 +0000

    Auto-submit and ajax topic select

--- js/app/auto-submit-form.js
+++ js/app/auto-submit-form.js
@@ -19,9 +19,8 @@ $(function() {
     var formCheckboxContainer = form.find('.js-checkbox-container'); //static container needed for delegated event handlebars
     var formCheckboxes = formCheckboxContainer.find('input[type="checkbox"]');
     var formKeywords = form.find('#input-keywords');
-    var formSelect = $('#select');
-    var atoz = $('.filters__a-z')
-    //var atozActive; //Stores the currently active letter in A-Z
+    var formSelect = $('select');
+    var formAtoZ = $('.filters__a-z');
 
     //Bind change/click events
     $(formDates).each(function () {
@@ -36,57 +35,17 @@ $(function() {
     $(formSelect).change(function() {
         submitForm();
     });
-    //$(atoz).each(function() {
-    //    var $this = $(this);
-    //    var parentItem = $this.closest('li');
-    //    //Set active item so a-z keyword search works from first load
-    //    if (parentItem.hasClass('filters__a-z-list-item--active')) {
-    //        atozActive = $(this).attr('href');
-    //    }
-    //    //Set active letter, remove class from previous active, load new results into page and add active class to new letter
-    //
-    //    //$(this).click(function(e) {
-    //    //    e.preventDefault();
-    //    //    atozActive = $this.attr('href');
-    //    //    loadNewResults(atozActive);
-    //    //    //if input contains keywords include query in Ajax
-    //    //    //if ($('#input-keywords').val()) {
-    //    //    //    loadNewResults(atozActive + "&query="  + $('#input-keywords').val())
-    //    //    //} else {
-    //    //    //    loadNewResults(atozActive);
-    //    //    //}
-    //    //    //$(atoz).each(function() {
-    //    //    //    var parentItem = $(this).closest('li');
-    //    //    //    if (parentItem.hasClass('filters__a-z-list-item--active')) {
-    //    //    //        parentItem.removeClass('filters__a-z-list-item--active');
-    //    //    //    }
-    //    //    //});
-    //    //    //$(parentItem).addClass('filters__a-z-list-item--active');
-    //    //});
-    //});
-
-    $(atoz).on('change', '.filters__a-z-list input', function(e) {
+    $(formAtoZ).on('change', '.filters__a-z-list input', function() {
         submitForm();
     });
 
     //Bind form submission to store form data and run ajax function
     $(form).submit(function(e) {
         e.preventDefault();
-        //If includes a-z parameter then include it in URL to ajax
-        //if ((window.location.search.indexOf('az=') > -1) && (atozActive)) {
-        //    var url = atozActive + "&" + $(form).serialize();
-        //} else {
-            var url = (window.location.pathname) + '?' + $(form).serialize();
-        //}
-
-        //Detect descriptive text and replace with loading message
-        //if ($('form').find('.search-page__results-text').html()) {
-            $('.search-page__results-text').empty();
-            $('.search-page__results-text').append('Loading...');
-            loadNewResults(url);
-        //} else {
-        //    loadNewResults(url);
-        //}
+        var url = (window.location.pathname) + '?' + $(form).serialize();
+        $('.search-page__results-text').empty();
+        $('.search-page__results-text').append('Loading...');
+        loadNewResults(url);
     });
 
 })
