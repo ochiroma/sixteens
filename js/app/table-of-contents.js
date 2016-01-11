5db34ca5f6cf7d6b72df5deb7d1cc638aecf9a6f
Crispioso <crispin.merriman@gmail.com>
2016-01-11 13:50:37 +0000

    Only show print/PDF in sticky TOC if visible on page

--- js/app/table-of-contents.js
+++ js/app/table-of-contents.js
@@ -21,8 +21,10 @@ $(function() {
         $('.table-of-contents--sticky__wrap #stickySelectArea').append('<h2 class="table-of-contents--sticky__heading">Table of contents</h2>');
 
         //add in print options
-        var printStickyWrap = $('<div class="col col--md-15 col--lg-17 hide--mobile"><p class="text-right padding-top-md--0 padding-bottom-md--0 margin-bottom-md--1 print--hide"><a href="" id="" class="link-complex nojs-hidden js-enhance--show jsEnhancePrint">Print this page&nbsp;</a><span class="icon icon-print--dark-small"></span></p><p class="text-right padding-top-md--0 padding-bottom-md--1 margin-top-md--0 margin-bottom-md--0 print--hide js-enhance--show"><a href="' + pdfDownloadLink +' " class="link-complex">Download as PDF&nbsp;</a><span class="icon icon-download--dark-small"></span></p></div>');
-        $(printStickyWrap).insertAfter($('.table-of-contents--sticky__wrap .col'));
+        if ($('.js-print-pdf').length > 0) {
+            var printStickyWrap = $('<div class="col col--md-15 col--lg-17 hide--mobile"><p class="text-right padding-top-md--0 padding-bottom-md--0 margin-bottom-md--1 print--hide"><a href="" id="" class="link-complex nojs-hidden js-enhance--show jsEnhancePrint">Print this page&nbsp;</a><span class="icon icon-print--dark-small"></span></p><p class="text-right padding-top-md--0 padding-bottom-md--1 margin-top-md--0 margin-bottom-md--0 print--hide js-enhance--show"><a href="' + pdfDownloadLink + ' " class="link-complex">Download as PDF&nbsp;</a><span class="icon icon-download--dark-small"></span></p></div>');
+            $(printStickyWrap).insertAfter($('.table-of-contents--sticky__wrap .col'));
+        }
 
 
         //create select list of sections
