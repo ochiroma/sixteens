f06db6af815223c57b103820fabdd772182c62d1
Crispioso <crispin.merriman@gmail.com>
2016-01-19 15:21:04 +0000

    Fix mobile fiters bugs

--- js/app/show-and-hide.js
+++ js/app/show-and-hide.js
@@ -8,49 +8,54 @@
 // Credit goes to Heydon Pickering (http://heydonworks.com/practical_aria_examples/#progressive-collapsibles)
 
 function showHide(remove) {
-	$('.js-show-hide .js-show-hide__title').each(function (){
-		var $this = $(this);
+    var showHideTrigger = $('.js-show-hide .js-show-hide__title');
 
+    if (showHideTrigger.length > 0) {
+        showHideTrigger.each(function (i) {
+            var $this = $(this),
+                panel = $this.next(),
+                button = $this.children('button');
 
-            // create unique id for a11y relationship
-            var id = 'collapsible-' + $this.index();
+            if (!remove) {
 
-            if ($this.hasClass('is-shown')) {
-                var bool = false;
+                // create unique id for a11y relationship
+                var id = 'collapsible-' + $this.index();
+
+                if ($this.hasClass('is-shown')) {
+                    var bool = false;
+                } else {
+                    var bool = true;
+                }
+
+                // wrap the content and make it focusable
+                $this.nextUntil('.js-show-hide__title').attr({id: id, 'aria-hidden': bool});
+
+                // inverse the boolean value
+                bool = !bool;
+
+                // Add the button inside the title so both the heading and button semantics are read - if prevents multiple buttons being added
+                if (button.length == 0) {
+                    $this.wrapInner('<button class="js-show-hide__button" type="button" aria-expanded="' + bool + '" aria-controls="' + id + '">');
+                }
+                button = $this.children('button');
+
+                // Toggle the state properties on click
+                button.on('click', function () {
+                    var state = $(this).attr('aria-expanded') === 'false' ? true : false;
+                    $(this).attr('aria-expanded', state);
+                    panel.attr('aria-hidden', !state);
+                });
             } else {
-                var bool = true;
+                // Set filters to show
+                $this.children('button').attr('aria-expanded', 'true');
+                panel.attr('aria-hidden', 'false');
+
+                // Remove show/hide functionality
+                var heading = $this.children('button').contents();
+                heading.unwrap();
             }
-
-            // wrap the content and make it focusable
-            $this.nextUntil('.js-show-hide__title').attr({id: id, 'aria-hidden': bool});
-            var panel = $this.next();
-
-
-            // inverse the boolean value
-            bool = !bool;
-
-        if (!remove) {
-
-            // Add the button inside the title so both the heading and button semantics are read
-            $this.wrapInner('<button type="button" aria-expanded="' + bool + '" aria-controls="' + id + '">');
-            var button = $this.children('button');
-
-            // Toggle the state properties on click
-            button.on('click', function () {
-                var state = $(this).attr('aria-expanded') === 'false' ? true : false;
-                $(this).attr('aria-expanded', state);
-                panel.attr('aria-hidden', !state);
-            });
-        } else {
-            // Set filters to show
-            $this.children('button').attr('aria-expanded', 'true');
-            panel.attr('aria-hidden', 'false');
-
-            // Remove show/hide functionality
-            var heading = $this.children('button').contents();
-            heading.unwrap();
-        }
-	});
+        });
+    }
 }
 
 showHide();
