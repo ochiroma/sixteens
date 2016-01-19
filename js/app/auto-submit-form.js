1c41ca8641b29acedbeb8713deb9c0b691cec620
Crispioso <crispin.merriman@gmail.com>
2016-01-19 12:11:59 +0000

    Add/remove mobile filters on resize

--- js/app/auto-submit-form.js
+++ js/app/auto-submit-form.js
@@ -1,5 +1,5 @@
 
-$(function() {
+function autoSubmitForm() {
     //Function to submit form
     function submitForm(element) {
         var elementForm = $(element).closest(form);
@@ -8,38 +8,41 @@ $(function() {
 
     //Variables
     var form = '.js-auto-submit__form',
-    input = '.js-auto-submit__input',
-    $keywordSearch = $('input[type="search"].js-auto-submit__input'),
-    $clearAll = $(form).find('a[value="Reset"]'),
-    $atozContainer = $('.js-atoz-container'),
-    $selectUpdated = $('#select-updated'),
-    url,
-    timer;
+        input = '.js-auto-submit__input',
+        $keywordSearch = $('input[type="search"].js-auto-submit__input'),
+        $clearAll = $(form).find('a[value="Reset"]'),
+        $atozContainer = $('.js-atoz-container'),
+        $selectUpdated = $('#select-updated'),
+        url,
+        timer;
+
+    // Hide submit button
+    $('.js-submit-button').hide();
 
     // Keyword search auto-submit
-    $(form).on('keyup', $keywordSearch, function() {
+    $(form).on('keyup', $keywordSearch, function () {
         // Timed to allow for typing to finish
         var $this = $(this);
         clearTimeout(timer);
-        timer = setTimeout(function(){
+        timer = setTimeout(function () {
             submitForm($this);
         }, 500);
     });
-    $(form).on('paste search', $keywordSearch, function() {
+    $(form).on('paste search', $keywordSearch, function () {
         // Submit instantly on paste/clear
         var $this = $(this);
         submitForm($this);
     });
 
     // Auto-submit instantly for all other elements
-    $(form).on('change', input, function(e) {
+    $(form).on('change', input, function (e) {
         var $target = $(e.target);
         var $targetId = $(e.target).attr('id');
         if ($targetId !== $keywordSearch.attr('id') && $targetId !== 'select-updated') { //Don't submit again after keyword change
             submitForm($target);
         } else if ($targetId == $selectUpdated.attr('id')) { //Clear custom dates on timeseries tool if 'Custom' not selected
             if ($selectUpdated.val() != 'custom') {
-                $('#input-start-date, #input-start-date').each(function(){
+                $('#input-start-date, #input-start-date').each(function () {
                     $(this).val('');
                 });
             }
@@ -48,7 +51,7 @@ $(function() {
     });
 
     //Bind clear form click event
-    $clearAll.click(function(e) {
+    $clearAll.click(function (e) {
         e.preventDefault();
         url = $(this).attr('href');
         loadNewResults(url, true);
@@ -56,18 +59,25 @@ $(function() {
 
     //Auto-submit A-Z filters
     if ($atozContainer.length > 0) {
-        $atozContainer.on('change', '.filters__a-z input', function(e) {
+        $atozContainer.on('change', '.filters__a-z input', function (e) {
             var $target = $(e.target);
             submitForm($target);
         });
     }
 
     //Bind form submission to store form data and run ajax function
-    $(form).submit(function(e) {
+    $(form).submit(function (e) {
         e.preventDefault();
         url = (window.location.pathname) + '?' + $(form).serialize();
         loadNewResults(url);
         return false;
     });
 
+}
+
+
+$(function() {
+    if (!$('body').hasClass('viewport-sm')) { // on medium viewport and up auto-submit filters form
+        autoSubmitForm();
+    }
 });
--- js/app/show-and-hide.js
+++ js/app/show-and-hide.js
@@ -7,37 +7,49 @@
 
 // Credit goes to Heydon Pickering (http://heydonworks.com/practical_aria_examples/#progressive-collapsibles)
 
-function showHide() {
+function showHide(remove) {
 	$('.js-show-hide .js-show-hide__title').each(function (){
 		var $this = $(this);
 
-		// create unique id for a11y relationship
-		var id = 'collapsible-' + $this.index();
 
-		if ($this.hasClass('is-shown')) {
-			var bool = false;
-		} else {
-			var bool = true;
-		}
+            // create unique id for a11y relationship
+            var id = 'collapsible-' + $this.index();
 
-		// wrap the content and make it focusable
-		$this.nextUntil('.js-show-hide__title').attr({id: id, 'aria-hidden': bool});
-		var panel = $this.next();
+            if ($this.hasClass('is-shown')) {
+                var bool = false;
+            } else {
+                var bool = true;
+            }
+
+            // wrap the content and make it focusable
+            $this.nextUntil('.js-show-hide__title').attr({id: id, 'aria-hidden': bool});
+            var panel = $this.next();
 
 
-		// inverse the boolean value
-		bool = !bool;
+            // inverse the boolean value
+            bool = !bool;
 
-		// Add the button inside the title so both the heading and button semantics are read
-		$this.wrapInner('<button type="button" aria-expanded="' + bool +'" aria-controls="' + id + '">');
-		var button = $this.children('button');
+        if (!remove) {
 
-		// Toggle the state properties on click
-		button.on('click', function() {
-			var state = $(this).attr('aria-expanded') === 'false' ? true : false;
-			$(this).attr('aria-expanded', state);
-			panel.attr('aria-hidden', !state);
-		});
+            // Add the button inside the title so both the heading and button semantics are read
+            $this.wrapInner('<button type="button" aria-expanded="' + bool + '" aria-controls="' + id + '">');
+            var button = $this.children('button');
+
+            // Toggle the state properties on click
+            button.on('click', function () {
+                var state = $(this).attr('aria-expanded') === 'false' ? true : false;
+                $(this).attr('aria-expanded', state);
+                panel.attr('aria-hidden', !state);
+            });
+        } else {
+            // Set filters to show
+            $this.children('button').attr('aria-expanded', 'true');
+            panel.attr('aria-hidden', 'false');
+
+            // Remove show/hide functionality
+            var heading = $this.children('button').contents();
+            heading.unwrap();
+        }
 	});
 }
 
