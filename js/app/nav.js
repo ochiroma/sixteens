00b815b823e57f8775bda77f0fa85372e3ebd30e
Crispioso <crispin.merriman@gmail.com>
2016-02-03 10:27:14 +0000

    Add extra keyboard accessibility to main nav

--- js/app/nav.js
+++ js/app/nav.js
@@ -74,7 +74,6 @@ function cloneSecondaryNav() {
         // Remove from primary nav and add into separate secondary list
         $navItem.each(function(i) {
             var index = i + 1;
-            console.log(index);
            $(this)
                .unwrap()
                .removeClass('primary-nav__link col')
@@ -101,6 +100,7 @@ $(window).resize(function() {
 $(document).ready(function () {
     var $primaryNav = $('#nav-primary');
     var $searchBar = $('#searchBar');
+    var $navItem = $('.js-nav');
 
     cloneSecondaryNav();
 
@@ -121,46 +121,112 @@ $(document).ready(function () {
         event.stopPropagation();
     });
 
-    $('.js-expandable').on('keydown', function (event) {
-        console.log(event.keyCode);
-        // Enter || spacebar
-        if (event.keyCode === 13 || event.keyCode === 32) {
-            if ($(window).width() < 767) {
-                event.preventDefault();
-                toggleSubnav($(this));
+    // Menu navigation using keyboard
+    $navItem.on('keydown', function(e) {
+        var $this = $(this),
+            $focusedItem = $('.js-expandable__child a:focus'), // only selects child item that is in focus
+            keycode = e.keyCode,
+            up = '38',
+            down = '40',
+            right = '39',
+            left = '37',
+            esc = '27',
+            tab = '9';
+        if (keycode == tab && $focusedItem) {
+            $this.removeClass('primary-nav__item--focus');
+            $this.next().focus();
+        }
+        if (keycode == esc) {
+            $this.removeClass('primary-nav__item--focus');
+            $this.closest('.js-nav').find('a:first').addClass('hide-children').focus();
+            $this.closest('.js-nav').find('a:first').focusout(function() {
+               $(this).removeClass('hide-children');
+            });
+        }
+        if (keycode == down) {
+            e.preventDefault();
+            $this.addClass('primary-nav__item--focus');
+            if ($focusedItem.length > 0) {
+                $focusedItem.parent().next().find('a').focus();
+            } else {
+                $this.find('.js-expandable__child:first a').focus();
             }
         }
-
-        // Right arrow
-        if (event.keyCode === 39) {
-            if ($(window).width() < 767) {
-                event.preventDefault();
-                expandSubnav($(this));
-            }
-            else {
-                // TODO: Make sure the large viewport breakpoint is exact
-                // This should move the focus to the next nav item and expand its subnav
-                //event.preventDefault();
-                //var nextNode = $(this).closest('.js-expandable').next();
-                //$(nextNode).focus();
+        if (keycode == up) {
+            e.preventDefault();
+            if ($focusedItem.length > 0 && $focusedItem.parent().prev().length > 0) {
+                $focusedItem.parent().prev().find('a').focus();
+            } else {
+                $this.removeClass('primary-nav__item--focus');
+                $this.find('a:first').focus();
             }
         }
-
-        // Left arrow
-        if (event.keyCode === 37) {
-            if ($(window).width() < 767) {
-                event.preventDefault();
-                collapseSubnav($(this));
-            }
-            else {
-                // TODO: Make sure the large viewport breakpoint is exact
-                // This should move the focus to the previous item and expand its subnav
-                //event.preventDefault();
-                //var previousNode = $(this).closest('.js-expandable').prev();
-                //$(previousNode).focus();
-            }
+        if (keycode == right) {
+            e.preventDefault();
+            $this.removeClass('primary-nav__item--focus');
+            $this.closest('.js-nav').next().find('a:first').focus();
+        }
+        if (keycode == left) {
+            e.preventDefault();
+            $this.removeClass('primary-nav__item--focus');
+            $this.closest('.js-nav').prev().find('a:first').focus();
         }
     });
+
+    // Remove focus and styling classes if mouse hovers over nav
+    $navItem.hover(function() {
+        if ($navItem.find(':focus')) {
+            $navItem.find(':focus').blur();
+            $navItem.removeClass('primary-nav__item--focus');
+        }
+    });
+
+    // Close menu on click of the page
+    $('body').not('js-expandable .js-expandable__child').click(function(e) {
+        e.stopPropagation();
+        $('.primary-nav__item--focus').removeClass('primary-nav__item--focus');
+    });
+
+
+    //$('.js-expandable > a').on('keydown', function (event) {
+    //    // Enter || spacebar
+    //    if (event.keyCode === 13 || event.keyCode === 32) {
+    //        if ($(window).width() < 767) {
+    //            event.preventDefault();
+    //            toggleSubnav($(this));
+    //        }
+    //    }
+    //
+    //    // Right arrow
+    //    if (event.keyCode === 39) {
+    //        if ($(window).width() < 767) {
+    //            event.preventDefault();
+    //            expandSubnav($(this));
+    //        }
+    //        else {
+    //            // TODO: Make sure the large viewport breakpoint is exact
+    //            // This should move the focus to the next nav item and expand its subnav
+    //            //event.preventDefault();
+    //            //var nextNode = $(this).closest('.js-expandable').next();
+    //            //$(nextNode).focus();
+    //        }
+    //    }
+    //
+    //    // Left arrow
+    //    if (event.keyCode === 37) {
+    //        if ($(window).width() < 767) {
+    //            event.preventDefault();
+    //            collapseSubnav($(this));
+    //        }
+    //        else {
+    //            // TODO: Make sure the large viewport breakpoint is exact
+    //            // This should move the focus to the previous item and expand its subnav
+    //            //event.preventDefault();
+    //            //var previousNode = $(this).closest('.js-expandable').prev();
+    //            //$(previousNode).focus();
+    //        }
+    //    }
+    //});
 
     // stop parent element from taking over enter/space events
     $('.js-expandable > .nav--primary__sub').on('keydown', function (event) {
@@ -203,51 +269,51 @@ $(document).ready(function () {
     // taken from Simply Accessible — thank you kindly.
     // http://simplyaccessible.com/article/better-for-accessibility
 
-    $(function () {
-        $primaryNav.setup_navigation();
-    });
-
-    $.fn.setup_navigation = function (settings) {
-        settings = jQuery.extend({
-            focusClass: 'menu-focus',
-        }, settings);
-
-
-        // Set tabIndex to -1 so that links can't receive focus until menu is open
-        $(this).find('> li > a').next('ul').find('a').attr('tabIndex', -1);
-
-        $(this).find('> li > a').hover(function () {
-            $(this).closest('ul')
-                .find('.' + settings.focusClass).removeClass(settings.focusClass)
-                .find('a').attr('tabIndex', -1);
-        });
-        $(this).find('> li > a').focus(function () {
-            $(this).closest('ul')
-                .find('.' + settings.focusClass).removeClass(settings.focusClass)
-                .find('a').attr('tabIndex', -1);
-            $(this).next('ul')
-                .addClass(settings.focusClass)
-                .find('a').attr('tabIndex', 0);
-        });
-
-        // Hide menu if click or focus occurs outside of navigation
-        $(this).find('a').last().keydown(function (e) {
-            if (e.keyCode === 9) {
-                // If the user tabs out of the navigation hide all menus
-                $('.' + settings.focusClass)
-                    .removeClass(settings.focusClass)
-                    .find('a').attr('tabIndex', -1);
-            }
-        });
-        $(document).click(function () {
-            $('.' + settings.focusClass)
-                .removeClass(settings.focusClass)
-                .find('a').attr('tabIndex', -1);
-        });
-
-        $(this).click(function (e) {
-            e.stopPropagation();
-        });
-    };
+    //$(function () {
+    //    //$('.primary-nav__list').setup_navigation();
+    //});
+    //
+    //$.fn.setup_navigation = function (settings) {
+    //    settings = jQuery.extend({
+    //        focusClass: 'menu-focus',
+    //    }, settings);
+    //
+    //
+    //    // Set tabIndex to -1 so that links can't receive focus until menu is open
+    //    $(this).find('> li > a').next('ul').find('a').attr('tabIndex', -1);
+    //
+    //    $(this).find('> li > a').hover(function () {
+    //        $(this).closest('ul')
+    //            .find('.' + settings.focusClass).removeClass(settings.focusClass)
+    //            .find('a').attr('tabIndex', -1);
+    //    });
+    //    $(this).find('> li > a').focus(function () {
+    //        $(this).closest('ul')
+    //            .find('.' + settings.focusClass).removeClass(settings.focusClass)
+    //            .find('a').attr('tabIndex', -1);
+    //        $(this).next('ul')
+    //            .addClass(settings.focusClass)
+    //            .find('a').attr('tabIndex', 0);
+    //    });
+    //
+    //    // Hide menu if click or focus occurs outside of navigation
+    //    $(this).find('a').last().keydown(function (e) {
+    //        if (e.keyCode === 9) {
+    //            // If the user tabs out of the navigation hide all menus
+    //            $('.' + settings.focusClass)
+    //                .removeClass(settings.focusClass)
+    //                .find('a').attr('tabIndex', -1);
+    //        }
+    //    });
+    //    $(document).click(function () {
+    //        $('.' + settings.focusClass)
+    //            .removeClass(settings.focusClass)
+    //            .find('a').attr('tabIndex', -1);
+    //    });
+    //
+    //    $(this).click(function (e) {
+    //        e.stopPropagation();
+    //    });
+    //};
 
 });
