19665efb205c4011730fe81db33e7ff05d498ff2
Jon Dewi Jones <jondewijones@gmail.com>
2016-02-19 16:22:08 +0000

    Fixed typo

--- js/app/js-enhance.js
+++ js/app/js-enhance.js
@@ -471,16 +471,16 @@ function jsEnhanceMobileTables() {
 
 function jsEnhanceMobileCharts() {
 
-    // if on mobile inject overlay and button elemetns
+    // if on mobile inject overlay and button elements
     if ($("body").hasClass("viewport-sm")) {
         $('<div class="markdown-chart-overlay"></div>').insertAfter($('.markdown-chart'));
         $('<button class="btn btn--mobile-chart-show">View chart</button>').insertAfter($('.markdown-chart'));
         $('<button class="btn btn--mobile-chart-hide">Close chart</button>').appendTo($('.markdown-chart-overlay'));
 
-        $('.btn--mobile-chart-show').click(function(e) {
+        $('.btn--mobile-chart-show').click(function() {
             // the variables
             var $this = $(this),
-                $title = '<span class="font-size--h4">' + $this.closest('.markdown-chart-container').find('h4').text() + '</span>',
+                $title = $('<span class="font-size--h4">' + $this.closest('.markdown-chart-container').find('h4').text() + '</span>'),
                 $imgSrc = $this.closest('.markdown-chart-container').find('.js-chart-image-src').attr('href'),
                 width = 700,
                 $img = '<img src="' + $imgSrc + '&width=' + width +'" />',
@@ -496,7 +496,7 @@ function jsEnhanceMobileCharts() {
             $overlay.show();
         });
 
-        $('.btn--mobile-chart-hide').click(function(e) {
+        $('.btn--mobile-chart-hide').click(function() {
             $(this).closest('.markdown-chart-overlay').css('display', '');
         });
     }
