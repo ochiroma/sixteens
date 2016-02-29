3118bf6927156e41f3c6e8f24e3bc106c361350e
Crispioso <crispin.merriman@gmail.com>
2016-02-29 17:34:39 +0000

    Move language detect on load to Babbage

--- js/app/language-toggle.js
+++ js/app/language-toggle.js
@@ -1,12 +1,7 @@
-$(function() {
-    // Variables
-    var domain = window.location.host,
-        langSelect = $(".language.language--js select");
 
-    // Detect which language is selected and hide other
-    if (domain.match("^cy.")) {
-        langSelect.val('welsh');
-    }
+$(function() {
+    // Find language <select> markup
+    var langSelect = $(".language.language--js select");
 
     // Listen for change of select and navigate to new url
     langSelect.change(function() {
