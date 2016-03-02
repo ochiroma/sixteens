4f39fe564690afc4ca08658a8bd48874d5265c00
Crispioso <crispin.merriman@gmail.com>
2016-03-02 16:03:30 +0000

    Enable pym.js

--- js/app/pym-interactive.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by crispin on 10/12/2015.
- */
-$(function() {
-    $('div.pym-interactive').each(function(index, element) {
-        new pym.Parent($(element).attr('id'), $(element).data('url'));
-    });
-});
