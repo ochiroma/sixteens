e9aece080b201ac5057ac0b619f82981056ab348
boxadesign <info@boxadesign.co.uk>
2017-06-21 16:24:40 +0100

    More code for the fllter list. Added the show - hide meta code

--- js/app/checkboxes.js
+++ js/app/checkboxes.js
@@ -1,45 +1,64 @@
 $(function() {
-    var selectEl = $('.js--select-all-none').find('span'),
+    var selectEl = $('.js--select-all-none').find('button'),
    		checkBoxGroup = $('.checkbox-group'),
    		checkBox = $('input', checkBoxGroup);
 
+   	// Select button is clicked.
     selectEl.on('click', function(){
     	var el = $(this),
-    		selectVal = el.attr('data-select'),
+    		selectVal = el.attr('aria-disabled'),
     		selectID = el.attr('ID'),
     		selectWrap = el.parent();
 
-    	if(selectVal == 'true') {
+    	// Check if button is disabled.
+    	if(selectVal == 'false') {
+    		// Select all checkboxes.
     		if(selectID == 'select-all'){
     			selectWrap.next(checkBoxGroup).find('input').prop('checked', true);
     		}
+    		// Deselect all checkboxes.
     		if(selectID == 'select-none'){
     			selectWrap.next(checkBoxGroup).find('input').prop('checked', false);
     		}
+			// Run the function to check how many checkboxes are checked.
     		checkCheckBoxes();
     	}
+
+    	return false;
     });
 
+    // Each time a checkbox is clicked run the fuction to check how many checkboxes are checked.
     checkBox.on('click', function(){
 		checkCheckBoxes();
     });
 
+    // Function to monitor how many checkboxes are checked.
     function checkCheckBoxes(){
-    	checkBoxGroup.each(function(){
+    	// Loop through each instance of a checkbox grouping (so we can have more than one).
+    	checkBoxGroup.each(function(i){
     		var relatedSelector = $(this).prev(),
     			selectAll = relatedSelector.find('#select-all'),
     			selectNone = relatedSelector.find('#select-none');
 
+            // create unique id for aria relationship and add to the button wrapper and each checkbox group.
+            var ariaId = 'checkboxes-' + i;
+            $(this).attr('id', ariaId);
+            $(relatedSelector).attr('id', ariaId);
+    		
+    		// When all checkboxes are checked.
 	    	if ($('input:checked', this).length == $('input', this).length) {
-				selectAll.attr('data-select', 'false');
-				selectNone.attr('data-select', 'true');
+				selectAll.attr('aria-disabled', 'true').addClass('btn--link-disabled');
+				selectNone.attr('aria-disabled', 'false').removeClass('btn--link-disabled');
+	    	// When no checkboxes are checked.
 			} else if ($('input:checked', this).length == 0) {
-				selectNone.attr('data-select', 'false');
-				selectAll.attr('data-select', 'true');
+				selectNone.attr('aria-disabled', 'true').addClass('btn--link-disabled');
+				selectAll.attr('aria-disabled', 'false').removeClass('btn--link-disabled');
+			// When some checkboxes are checked.
 			} else {
-				selectEl.attr('data-select', 'true');
+				selectEl.attr('aria-disabled', 'false').removeClass('btn--link-disabled');
 			}
 		});
 	}
+	// Run on page load.
 	checkCheckBoxes();
 });
