0b5fa7213e60dcd8c6d80b9de35cb12878b27b1d
Jon Dewi Jones <jondewijones@gmail.com>
2017-07-11 08:48:36 +0100

    Add push to dataLayer on selection

--- js/app/table-of-contents.js
+++ js/app/table-of-contents.js
@@ -56,6 +56,7 @@ $(function() {
 
         $('.table-of-contents--sticky__select').change(function() {
             var location = $(this).find('option:selected').val();
+            var title = $(this).find('option:selected').text()
             if (location) {
                 // expands section if show/hide
                 forceShow($(location));
@@ -86,8 +87,18 @@ $(function() {
                         functionTrigger = false;
                     }
                 });
+
+                // Check if GTM dataLayer exists and if it does push
+                // a click event with the title of element clicked
+                if (window.dataLayer != undefined) {
+                    window.dataLayer.push({
+                        'event': 'StickyTopNavCLick',
+                        'stickyTopNav': title
+                    });
+                }
             }
         });
+
 
 
         // sticky toc function that evaluates scroll position and activates the sticky toc as appropriate
