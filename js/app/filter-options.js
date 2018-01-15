93625fba1194c47fedd5e776f0739cc64bebfc5d
mattrout92 <matt_rout92@live.co.uk>
2018-01-15 15:36:33 +0000

    Minor updates for mobile filter options screen

--- js/app/filter-options.js
+++ js/app/filter-options.js
@@ -16,6 +16,9 @@ $(document).ready(function() {
                 if (errorDimensions.length > 0) {
                     label = " " + label;
                 }
+                var options = $(this).find("div.block-with-text span");
+                options.html("Add at least one");
+                options.addClass("form-error");
                 errorDimensions.push(label);
             }
         })
@@ -23,10 +26,10 @@ $(document).ready(function() {
         if (errorDimensions.length > 0 && $("#options-error").length === 0) {
             var landingPageUrl = $("a.breadcrumb__link").attr("href");
 
-            $("#error-container").append("<div id=\"options-error\" class=\"font-size--16 form-error filter-overview__error-message margin-bottom--1\">Add at least one to '" + errorDimensions + "' to generate data</div>")
+            $("#error-container").append("<div id=\"options-error\" class=\"font-size--16 form-error filter-overview__error-message margin-bottom--1\">Add at least one filter to '" + errorDimensions + "' to generate data</div>")
             $("#error-container").append("<div class=\"font-size--16 margin-bottom--4\"> Alternatively, return to the <a href=\""+landingPageUrl+"\">landing page</a> to download the complete dataset.</div>")
         }
-    })
+    }) 
 
     if ($("li#filter-option.filter-overview__add").length > 0) {
         $("#preview-download").addClass("btn--primary-disabled");
