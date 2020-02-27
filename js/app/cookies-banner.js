97f680704aba38c892cdc54111008192e1ac1e51
Ravi Pradhan <ravipradhan@protonmail.com>
2020-02-27 09:36:59 +0000

    updated styling for 'Hide' button

--- js/app/cookies-banner.js
+++ js/app/cookies-banner.js
@@ -1,7 +1,4 @@
-// enhanced cookie banner needs to:
-// 1. on 'Accept All', send a request to the cookies controller designating that all cookies are to be accepted
-// 2. set cookies_preferences_set=true; cookies_policy={essential: true, usage: true}
-// 3. Success message
+var oneYearInSeconds = 31622400
 
 function initCookiesBanner() {
     $('.js-hide-cookies-banner').click(function(e) {
@@ -14,21 +11,12 @@ function submitCookieForm(e) {
     e.preventDefault();
     $('.js-accept-cookies').prop('disabled')
     $('.js-accept-cookies').addClass("btn--primary-disabled")
-    $.ajax({
-        type: "GET",
-        url: "/cookie/accept-all",
-        error: function (error) {
-            console.log('Error with accepting all cookies:', error.statusText);
-            $('.js-accept-cookies').removeProp('disabled')
-            $('.js-accept-cookies').removeClass("btn--primary-disabled")
-        },
-        success: function(result) {
-            // to update once endpoint is available
-            document.cookie = result.cookies_policy
-            $('.js-cookies-banner-inform').addClass('hidden')
-            $('.js-cookies-banner-confirmation').removeClass('hidden')
-        }
-    });
+
+    document.cookie = "cookies_preferences_set=true; max-age=" + oneYearInSeconds + ";"
+    document.cookie = "cookies_policy=%7B%22essential%22%3Atrue%2C%22usage%22%3Atrue%7D; max-age=" + oneYearInSeconds + ";"
+
+    $('.js-cookies-banner-inform').addClass('hidden')
+    $('.js-cookies-banner-confirmation').removeClass('hidden')
 }
 
 $(function() {
