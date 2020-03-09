8ef2da3bde9a41c6839491fb467f84184d888251
Daniel W <danieldwalford@gmail.com>
2020-03-09 10:02:01 +0000

    Cookies preferences page updated with a no Js version
    This makes it possible to change the content on the cookies pref page to show / hide elements depending on if JS is enabled or disabled.

--- js/app/cookies-pref.js
+++ js/app/cookies-pref.js
@@ -1,6 +1,9 @@
-$(function() {
-    if($("#cookies-no-js").length > 0) {
-        $("#cookies-no-js").addClass('hidden');
+$(function () {
+    if ($(".cookies-no-js").length > 0) {
+        $(".cookies-no-js").addClass('hidden');
+    }
+    if ($(".cookies-js").length > 0) {
+            $(".cookies-js").removeClass('hidden');
     }
 });
 
