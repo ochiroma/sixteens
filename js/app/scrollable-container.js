7e7ea09792d57ced969d7d9e9eb2882c618fa742
Tom Andrews <git@tomandrews.co.uk>
2020-08-26 16:18:42 +0100

    Hide and show shadows as necessary

--- js/app/scrollable-container.js
+++ js/app/scrollable-container.js
@@ -1,5 +1,24 @@
 'use strict';
 $(function () {
+    function showHideShadows(element) {
+        var scrollLeft = element.scrollLeft;
+        var $element = $(element);
+        if (scrollLeft > 0) {
+            $element.addClass('scroll-container__left-shadow')
+        }
+        else {
+            $element.removeClass('scroll-container__left-shadow')
+        }
+
+        var scrollLeftMax = element.scrollWidth - element.clientWidth;
+        if (scrollLeft === scrollLeftMax) {
+            $element.removeClass('scroll-container__right-shadow')
+        }
+        else {
+            $element.addClass('scroll-container__right-shadow')
+        }
+    }
+
     var observer = new ResizeObserver(function (entries) {
         for (entry of entries) {
             var container = entry.target;
@@ -7,10 +26,14 @@ $(function () {
             var hasOverflow = container.scrollWidth > $container.width();
             if (!hasOverflow) {
                 $container.removeClass('scrollable-container__overflow');
+                $container.off('scroll');
                 continue;
             }
-            console.log(entry.contentRect.height + 10);
             $container.addClass('scrollable-container__overflow');
+            showHideShadows(container);
+            $container.on('scroll', function (event) {
+                showHideShadows(event.target)
+            });
         }
     });
     $('.scrollable-container').each(function (i, container) {
