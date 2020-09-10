9035b6726ea9268aedcd55855f0167f2d988c57a
Tom Andrews <git@tomandrews.co.uk>
2020-09-10 14:06:45 +0100

    Ensure we only add this to the page footer not any others

--- js/app/viewport-size.js
+++ js/app/viewport-size.js
@@ -1,5 +1,10 @@
 $(function() {
-    $("footer").append("<div id='viewport-sm' class='js-viewport-size'></div>" +
+    var footers = $('footer');
+    if (footers.length == 0) {
+        return;
+    }
+    // Get the last one expected to be the main one for the page
+    $(footers.get(footers.length - 1)).append("<div id='viewport-sm' class='js-viewport-size'></div>" +
         "<div id='viewport-md' class='js-viewport-size'></div>" +
         "<div id='viewport-lg' class='js-viewport-size'></div>");
 
